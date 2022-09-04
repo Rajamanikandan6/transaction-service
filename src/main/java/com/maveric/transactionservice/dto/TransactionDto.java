0995e96aa8cd9354d9e37d422761846d74425083
MAVERICSYSTEMS\supritask <supritask@maveric-systems.com>
2022-09-04 09:37:12 +0530

    Added dependencies

--- src/main/java/com/maveric/transactionservice/dto/TransactionDto.java
+++ src/main/java/com/maveric/transactionservice/dto/TransactionDto.java
@@ -5,14 +5,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class TransactionDto {
 
     private String _id;
+    @NotNull(message = "Account Id is mandatory")
     private String accountId;
+    @NotNull(message = "Type is mandatory - 'CREDIT' or 'DEBIT'")
     private String type;
+    @NotNull(message = "Amount is mandatory")
+    @Min(value=0,message = "Amount cannot be less than zero")
     private String amount;
     private String createdAt;
 }
